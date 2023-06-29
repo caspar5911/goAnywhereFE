d9a908fb97e85bbd53dc7d45000a47dcda29db7a
Caspar Lee <caspar95galaxy@gmail.com>
2023-06-30 00:32:04 +0800

    feat: change for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/chat" element={<ChatComponent />} />
           <Route path="/itinerary" element={<ItineraryComponent />} />
-          <Route path="/" element={<TourismPage />} />
+          <Route path="/*" element={<TourismPage />} />
         </Routes>
       </div>
     </Router>
